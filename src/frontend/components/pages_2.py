e9e7339a7a4a45c6ed29f375249a2b9f5f094081
matthisarvois <arvois.matthis60@gmail.com>
2026-02-03 11:19:30 +0100

    premier ajout dans la branch

---
+++ src/frontend/components/pages_2.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+import polars as pl  
+from sklearn.datasets import load_iris
+import time
+import numpy as np
+import plotly.express as px
+
+
+def app() -> None:
+    return None
